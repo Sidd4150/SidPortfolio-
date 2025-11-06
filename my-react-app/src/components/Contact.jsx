d5ee68cf3a73d9be8ae25f995048d6dcb021afe8
Siddhartha Shakya <sshakya@dons.usfca.edu>
2025-11-06 12:02:59 -0800

    build

--- my-react-app/src/components/Contact.jsx
+++ my-react-app/src/components/Contact.jsx
@@ -4,6 +4,6 @@ export default function Contact() {
         <h1>Contact information</h1>
         <a>Email: Sidushakya@gmail.com</a>
         <br></br>
-        <a href="https://github.com/Sidd4150">GitHub: https://github.com/Sidd4150</a>
+        <a href="https://github.com/Sidd4150">GitHub: https://github.com/Sidd4150 </a>
     </>
 }
